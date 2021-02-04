0d5983708f8dcc3e68ef0e580ac8a4f7811c4c58
Rowan Molony <rowan.molony@codema.ie>
2021-02-04 12:45:04 +0000

    Link Small Areas to nearest Capacitymap station (#23)
    Customer stations are not included in ESB's Capacitymap
    stations.  This linkage likely apprpriate for all regions
    except Dublin City Centre (ESB-Codema exchange)

--- des/join.py
+++ des/join.py
@@ -1,6 +1,15 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial import cKDTree
+
+
 def join_nearest_points(gdA, gdB):
-    nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
-    nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+    nA = np.array(
+        list(gdA.geometry.apply(lambda x: (x.x, x.y)))
+    )
+    nB = np.array(
+        list(gdB.geometry.apply(lambda x: (x.x, x.y)))
+    )
     btree = cKDTree(nB)
     dist, idx = btree.query(nA, k=1)
     gdB_nearest = gdB.iloc[idx].drop(columns="geometry").reset_index(drop=True)
