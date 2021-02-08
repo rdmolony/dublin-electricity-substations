10ae02c8fbb1be733d8349df50193291f4c6b058
Rowan Molony <rowan.molony@codema.ie>
2021-02-08 16:41:13 +0000

    Plot & Get shortest path via Dask delayed (#29)
    Shortens compute time via parallelism

--- des/distance.py
+++ des/distance.py
@@ -1,5 +1,8 @@
+from dask import compute
 from dask import delayed
+from dask.diagnostics import ProgressBar
 import networkx as nx
+import numpy as np
 import pandas as pd
 from sklearn.neighbors import BallTree
 from tqdm import tqdm
@@ -39,7 +42,7 @@ def euclidean_dist_vec(y1, x1, y2, x2):
 
 def get_nearest_node(G, point):
     """
-    Find the nearest node to a point (adapted from osmnx).
+    Find the nearest node to a point.
 
     Return the graph node nearest to some (lng, lat) or (x, y) point
 
@@ -55,26 +58,18 @@ def get_nearest_node(G, point):
     -------
     int or tuple of (int, float)
         Nearest node ID
-
-    Adapted from
-    ------------
-    https://github.com/gboeing/osmnx/blob/master/osmnx/distance.py
     """
     # dump graph node coordinates into a pandas dataframe with x and y columns
     coords = (d[0] for d in G.nodes(data=True))
-    df = pd.DataFrame(coords, columns=["x", "y"])
+    target = pd.DataFrame(coords, columns=["x", "y"])
+    tree = BallTree(target[["x", "y"]].values, leaf_size=2)
 
-    # add columns to df for the (constant) coordinates of reference point
-    df["ref_x"] = point[0]
-    df["ref_y"] = point[1]
+    distance_nearest, id_nearest = tree.query(
+        np.array(point).reshape(1, -1),
+        k=1,  # The number of nearest neighbors
+    )
 
-    # calculate distances using euclid's formula for projected geometries
-    dists = euclidean_dist_vec(y1=df["ref_y"], x1=df["ref_x"], y2=df["y"], x2=df["x"])
-
-    # nearest node's ID is the index label of the minimum distance
-    index = dists.idxmin()
-
-    return (df.iloc[index].x, df.iloc[index].y)
+    return tuple(target.iloc[id_nearest.item()])
 
 
 def get_nearest_nodes(G, points):
@@ -111,7 +106,7 @@ def get_nearest_nodes(G, points):
     return [tuple(x) for x in target_coords.to_numpy()]
 
 
-def get_network_paths_between_points(G, orig_points, dest_points):
+def get_network_paths_between_points_recursively(G, orig_points, dest_points):
     """
     Find the nearest dest_point to each orig_point.
 
@@ -148,9 +143,7 @@ def get_network_paths_between_points(G, orig_points, dest_points):
                     target=(orig_node),
                 )
             except nx.exception.NetworkXNoPath:
-                # if no path to any target_nodes remove entire subgraph
-                Gs = [subgraph for subgraph in S if orig_node in subgraph][0]
-                G_copy.remove_nodes_from(list(Gs.nodes()))
+                remove_subgraph(G, S, orig_node)
             else:
                 paths.append(path)
                 solved = True
@@ -160,49 +153,73 @@ def get_network_paths_between_points(G, orig_points, dest_points):
 def remove_subgraph(G, S, orig_node):
     # if no path to any target_nodes remove entire subgraph
     Gs = [subgraph for subgraph in S if orig_node in subgraph][0]
-    G = G.remove_nodes_from(list(Gs.nodes()))
-    return G
+    return G.remove_nodes_from(list(Gs.nodes()))
 
 
-def get_network_paths_between_points_lazy(G, orig_points, dest_points):
+def get_network_paths_between_points(G, orig, dest):
     """
-    Find the nearest dest_point to each orig_point.
+    Find the nearest dest to each orig.
 
     Parameters
     ----------
     G : networkx.MultiDiGraph
         input graph
-    orig_points : pandas.DataFrame
+    orig : pandas.DataFrame
         The points for which we will find the nearest dest_point
-    dest_points : pandas.DataFrame
+    dest : pandas.DataFrame
         The points to be compared to orig_points
 
     Returns
     -------
     list of (tuple of (int, list of (coords))
-        Shortest paths from orig_point to nearest dest_point
+        Shortest paths from orig to nearest dest
 
     Adapted from
     ------------
     https://stackoverflow.com/questions/63690631/osmnx-shortest-path-how-to-skip-node-if-not-reachable-and-take-the-next-neares/63713539#63713539
     """
-    S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+    orig_points = pd.DataFrame(
+        {
+            "x": orig.geometry.centroid.x,
+            "y": orig.geometry.centroid.y,
+        }
+    )
+    dest_points = pd.DataFrame(
+        {
+            "x": dest.geometry.x,
+            "y": dest.geometry.y,
+        }
+    )
     paths = []
-    for orig_point in tqdm(orig_points.itertuples(), total=len(orig_points)):
-        G_copy = G.copy()
-        solved = False
-        while not solved:
-            orig_node = delayed(get_nearest_node)(G_copy, (orig_point.x, orig_point.y))
-            target_nodes = delayed(get_nearest_nodes)(G_copy, dest_points)
-            try:
-                path = delayed(nx.multi_source_dijkstra)(
-                    G_copy,
-                    sources=target_nodes,
-                    target=(orig_node),
-                )
-            except nx.exception.NetworkXNoPath:
-                delayed(remove_subgraph)(G=G_copy, S=S, orig_node=orig_node)
-            else:
-                paths.append(path)
-                solved = True
-    return paths
+    target_nodes = delayed(get_nearest_nodes)(G, dest_points)
+    for orig_point in orig_points.itertuples():
+        orig_node = delayed(get_nearest_node)(G, (orig_point.x, orig_point.y))
+        path = delayed(nx.multi_source_dijkstra)(
+            G,
+            sources=target_nodes,
+            target=(orig_node),
+        )
+        paths.append(path)
+
+    with ProgressBar():
+        shortest_paths = compute(*paths)
+
+    return shortest_paths
+
+
+def get_largest_subgraph(G):
+
+    nodes = max(nx.connected_components(G), key=len)
+    return G.subgraph(nodes)
+
+
+def get_large_subgraphs(G, size=5):
+
+    largest_components = [
+        component
+        for component in sorted(nx.connected_components(G), key=len, reverse=True)
+        if len(component) > 5
+    ]
+    return nx.compose_all(
+        G.subgraph(component) for component in tqdm(largest_components)
+    )
