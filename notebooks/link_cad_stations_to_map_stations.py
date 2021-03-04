aaab7d7fe354a6d6ed0081ad84c4135216245c60
Rowan Molony <rowan.molony@codema.ie>
2021-03-04 11:43:31 +0000

    Use pathlib paths & py:percent (#48)
    Pathlib paths are more robust than fstrings as they work
    in Windows as well as nix.  Py:percent can be run in an
    IDE like a Notebook

--- notebooks/link_cad_stations_to_map_stations.py
+++ notebooks/link_cad_stations_to_map_stations.py
@@ -1,55 +1,53 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.0
-#   kernelspec:
-#     display_name: 'Python 3.9.1 64-bit (''des'': conda)'
-#     metadata:
-#       interpreter:
-#         hash: a568a3391fa07a69dd36ed86d7f7005a7e6c42f5000f39f23663d14718557572
-#     name: python3
-# ---
+# %%
+# Uncomment if running on Google Colab
+# Click RESTART RUNTIME if prompted
+# !pip install git+https://github.com/codema-dev/esb-network-pylib
 
-# +
+# %%
 from os import listdir
+from pathlib import Path
+from shutil import unpack_archive
+
 import geopandas as gpd
+import matplotlib.patheffects as pe
+import matplotlib.pyplot as plt
 
 import esb
 
-data_dir = "../data"
-cad_data = "/home/wsl-rowanm/Data/ESBdata_20200124"
-# -
+data_dir = Path("../data")
+cad_data = Path("/home/wsl-rowanm/Data/ESBdata_20200124")
 
+# %% [markdown]
 # # Get Dublin LA boundaries
 
+# %%
 esb.download(
     url="https://zenodo.org/record/4446778/files/dublin_admin_county_boundaries.zip",
-    to_filepath=f"{data_dir}/dublin_admin_county_boundaries.zip",
+    to_filepath=str(data_dir / "dublin_admin_county_boundaries.zip"),
 )
-esb.unzip(
-    filename=f"{data_dir}/dublin_admin_county_boundaries.zip",
-    extract_dir=f"{data_dir}",
+unpack_archive(
+    data_dir / "dublin_admin_county_boundaries.zip",
+    data_dir,
 )
 
 
+# %%
 dublin_admin_county_boundaries = esb.read_dublin_admin_county_boundaries(
-    f"{data_dir}/dublin_admin_county_boundaries"
+    data_dir / "dublin_admin_county_boundaries"
 )
 
+# %% [markdown]
 # # Get 38kV, 110kV & 220kV stations from CAD data
 #
 # ... there is no 400kV station in Dublin
 
+# %% [markdown]
 # Must be downloaded from the Codema Google Shared Drive or <span style="color:red">**requested from the ESB**</span>
 
-hv_network_dirpath = f"{cad_data}/Dig Request Style/HV Data"
+# %%
+hv_network_dirpath = cad_data / "Dig Request Style" / "HV Data"
 hv_network_filepaths = [
-    f"{hv_network_dirpath}/{filename}" for filename in listdir(hv_network_dirpath)
+    hv_network_dirpath / filename for filename in listdir(hv_network_dirpath)
 ]
 cad_stations_ireland = esb.read_network(hv_network_filepaths, levels=[20, 30, 40])
 cad_stations_dublin = gpd.sjoin(
@@ -58,15 +56,19 @@ cad_stations_dublin = gpd.sjoin(
     op="within",
 ).drop(columns=["index_right", "COUNTYNAME"])
 
+
+# %% [markdown]
 # # Get Map stations
 
+# %%
 esb.download(
     url="https://esbnetworks.ie/docs/default-source/document-download/heatmap-download-version-nov-2020.xlsx",
-    to_filepath=f"{data_dir}/heatmap-download-version-nov-2020.xlsx",
+    to_filepath=str(data_dir / "heatmap-download-version-nov-2020.xlsx"),
 )
 
+# %%
 heatmap_stations_ireland = esb.read_heatmap(
-    f"{data_dir}/heatmap-download-version-nov-2020.xlsx"
+    data_dir / "heatmap-download-version-nov-2020.xlsx"
 )
 heatmap_stations_dublin = gpd.sjoin(
     heatmap_stations_ireland,
@@ -75,20 +77,20 @@ heatmap_stations_dublin = gpd.sjoin(
 ).drop(columns="index_right")
 heatmap_stations_dublin_hv = heatmap_stations_dublin.query("station_name != 'mv/lv'")
 
+# %% [markdown]
 # ## Link stations to nearest geocoded station
 
+# %%
 cad_stations_linked_to_heatmap = esb.join_nearest_points(
     cad_stations_dublin, heatmap_stations_dublin_hv
 )
 
+# %% [markdown]
 # # Plot CAD stations vs Heatmap stations
 #
 # ... Open `png` version of below plot locally (see [save](#save)) to zoom in
 
-# +
-import matplotlib.patheffects as pe
-import matplotlib.pyplot as plt
-
+# %%
 f, ax = plt.subplots(figsize=(100, 100))
 
 dublin_admin_county_boundaries.plot(ax=ax, facecolor="teal", edgecolor="white")
@@ -118,13 +120,30 @@ heatmap_stations_dublin_hv.apply(
 )
 
 plt.legend(["CAD", "Heat Map", "Capacity Map"], prop={"size": 50})
-# -
 
+# %% [markdown]
 # # Save
 
-f.savefig(f"{data_dir}/cad-stations-linked-to-nearest-heatmap-station.png")
+# %%
+f.savefig(data_dir / "cad-stations-linked-to-nearest-heatmap-station.png")
 
+# %%
 cad_stations_linked_to_heatmap.to_file(
-    f"{data_dir}/cad-stations-linked-to-nearest-heatmap-station.geojson",
+    data_dir / "cad-stations-linked-to-nearest-heatmap-station.geojson",
     driver="GeoJSON",
+)
+
+# %%
+cad_stations_linked_to_heatmap_lat_long = (
+    cad_stations_linked_to_heatmap.assign(
+        Latitude=lambda gdf: gdf.to_crs(epsg=4326).geometry.y,
+        Longitude=lambda gdf: gdf.to_crs(epsg=4326).geometry.x,
+    )
+    .loc[:, ["station_name", "Latitude", "Longitude"]]
+    .sort_values(["Latitude", "Longitude"])
+)
+
+cad_stations_linked_to_heatmap_lat_long.to_csv(
+    data_dir / "cad-stations-linked-to-nearest-heatmap-station.csv",
+    index=False,
 )
--- notebooks/link_small_areas_to_cad_stations_via_network.py
+++ notebooks/link_small_areas_to_cad_stations_via_network.py
@@ -1,76 +1,88 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
+# %% [markdown]
 # # Run `link_cad_stations_to_map_stations.ipynb` first so that Small Areas can be linked to Map stations via CAD stations
 
+# %% [markdown]
 # # Setup
 
-# +
+# %%
+# Uncomment if running on Google Colab
+# Click RESTART RUNTIME if prompted
+# !pip install git+https://github.com/codema-dev/esb-network-pylib
+
+# %%
 from os import listdir
+from pathlib import Path
+from shutil import unpack_archive
 
 import pandas as pd
 import geopandas as gpd
 import matplotlib.patheffects as pe
 import matplotlib.pyplot as plt
 import momepy
+import networkx as nx
 from shapely.geometry import box
 
 import esb
 
-data_dir = "../data"
-cad_data = "/home/wsl-rowanm/Data/ESBdata_20200124"
-show_plots = True
-# -
+data_dir = Path("../data")
+cad_data = Path("/home/wsl-rowanm/Data/ESBdata_20200124")
+show_plots = False
 
+# %% [markdown]
 # # Get Small Area boundaries
 
+# %%
+small_area_boundaries_filepath = (
+    data_dir
+    / "Small_Areas_Ungeneralised_-_OSi_National_Statistical_Boundaries_-_2015-shp"
+)
 esb.download(
     url="https://opendata.arcgis.com/datasets/c85e610da1464178a2cd84a88020c8e2_3.zip",
-    to_filepath=f"{data_dir}/Small_Areas_Ungeneralised_-_OSi_National_Statistical_Boundaries_-_2015-shp.zip",
+    to_filepath=str(small_area_boundaries_filepath.with_suffix(".zip")),
 )
-esb.unzip(
-    filename=f"{data_dir}/Small_Areas_Ungeneralised_-_OSi_National_Statistical_Boundaries_-_2015-shp.zip",
-    extract_dir=f"{data_dir}/Small_Areas_Ungeneralised_-_OSi_National_Statistical_Boundaries_-_2015-shp",
+unpack_archive(
+    small_area_boundaries_filepath.with_suffix(".zip"),
+    small_area_boundaries_filepath,
 )
 
-small_areas = esb.read_dublin_small_areas(f"{data_dir}/Small_Areas_Ungeneralised_-_OSi_National_Statistical_Boundaries_-_2015-shp")
+small_areas = esb.read_dublin_small_areas(small_area_boundaries_filepath)
 
+# %% [markdown]
 # # Get Local Authority boundaries
 
+# %%
 dublin_admin_county_boundaries = esb.read_dublin_admin_county_boundaries(
-    f"{data_dir}/dublin_admin_county_boundaries"
+    data_dir / "dublin_admin_county_boundaries"
 )
 
+# %% [markdown]
 # # Get MV Network and Get 38kV, 110kV & 220kV  stations
 #
 # ... there is no 400kV station in Dublin
 
-ireland_mv_index = esb.read_mv_index(f"{cad_data}/Ancillary Data/mv_index.dgn")
+# %%
+ireland_mv_index = esb.read_mv_index(cad_data / "Ancillary Data" / "mv_index.dgn")
 
-dublin_boundary = gpd.GeoSeries(box(695000, 715000, 740000, 771000)).rename("geometry").to_frame()
+# %%
+dublin_boundary = (
+    gpd.GeoSeries(box(695000, 715000, 740000, 771000)).rename("geometry").to_frame()
+)
 dublin_mv_index = gpd.sjoin(ireland_mv_index, dublin_boundary, op="within")
 dublin_mv_network_filepaths = [
-    f"{cad_data}/Dig Request Style/MV-LV Data/{index}.dgn" for index in dublin_mv_index.Text
+    cad_data / "Dig Request Style" / "MV-LV Data" / f"{index}.dgn"
+    for index in dublin_mv_index.Text
 ]
 
-mv_network_lines = esb.read_network(dublin_mv_network_filepaths, levels=[10, 11, 14]).reset_index(drop=True).explode()
+mv_network_lines = (
+    esb.read_network(dublin_mv_network_filepaths, levels=[10, 11, 14])
+    .reset_index(drop=True)
+    .explode()
+)
 
+# %%
 hv_network_filepaths = [
-    f"{cad_data}/Dig Request Style/HV Data/{filename}"
-    for filename in listdir(f"{cad_data}/Dig Request Style/HV Data/")
+    cad_data / "Dig Request Style" / "HV Data" / filename
+    for filename in listdir(cad_data / "Dig Request Style" / "HV Data")
 ]
 
 hv_stations_ireland = esb.read_network(hv_network_filepaths, levels=[20, 30, 40])
@@ -85,13 +97,17 @@ hv_stations_dublin = (
     .assign(station_id=lambda gdf: gdf.index)
 )
 
+# %%
 if show_plots:
-    
-    ax = dublin_admin_county_boundaries.plot(figsize=(15,15), facecolor="orange", edgecolor="orange")
+
+    ax = dublin_admin_county_boundaries.plot(
+        figsize=(15, 15), facecolor="orange", edgecolor="orange"
+    )
     mv_network_lines.plot(ax=ax)
     dublin_boundary.plot(ax=ax, facecolor="none", edgecolor="cyan")
     hv_stations_dublin.plot(ax=ax, color="black")
 
+# %% [markdown]
 # # Link Each Small Area Centroid to a Station via Network
 #
 # Use `networkx` to find the station that is closest along the network to each small area centroid:
@@ -100,47 +116,61 @@ if show_plots:
 # - Extract all stations and small areas near the network
 # - Trace the path from each small area centroid to the nearest station along the network
 
+# %%
 G = momepy.gdf_to_nx(mv_network_lines, approach="primal")
 
-G_largest = distance.get_largest_subgraph(G)
+# %%
+G_largest = esb.get_largest_subgraph(G)
 
+# %%
 G_largest_nodes, G_largest_edges, G_largest_sw = momepy.nx_to_gdf(
-    G_largest,
-    points=True,
-    lines=True,
-    spatial_weights=True
+    G_largest, points=True, lines=True, spatial_weights=True
 )
 
+# %%
 G_largest_edges_buffered = (
     G_largest_edges[["geometry"]]
-    .assign(geometry=lambda gdf: gdf.buffer(750))
-    .dissolve()
+    .assign(geometry=lambda gdf: gdf.buffer(750), x=0)
+    .dissolve(by="x")[["geometry"]]
 )
 
+# %%
 small_areas_near_g_largest = esb.centroids_within(small_areas, G_largest_edges_buffered)
 
-hv_stations_near_g_largest = gpd.sjoin(
-    hv_stations_dublin,
-    G_largest_edges_buffered,
-    op="within",
-).drop(columns="index_right").reset_index(drop=True)
+# %%
+hv_stations_near_g_largest = (
+    gpd.sjoin(
+        hv_stations_dublin,
+        G_largest_edges_buffered,
+        op="within",
+    )
+    .drop(columns="index_right")
+    .reset_index(drop=True)
+)
 
-hv_stations_snapped_to_g_largest = esb.snap_points_to_network(G_largest, hv_stations_near_g_largest)
+# %%
+hv_stations_snapped_to_g_largest = esb.snap_points_to_network(
+    G_largest, hv_stations_near_g_largest
+)
 
+# %%
 shortest_paths = esb.get_network_paths_between_points(
     G=G_largest,
     orig_points=small_areas_near_g_largest,
     dest_points=hv_stations_snapped_to_g_largest,
 )
 
+# %%
 if show_plots:
-    n = 0 # change 'n' to change the path to be displayed
+    n = 0  # change 'n' to change the path to be displayed
     f, ax = plt.subplots(figsize=(15, 15))
 
     dublin_admin_county_boundaries.plot(edgecolor="red", facecolor="orange", ax=ax)
 
     positions = {z: [z[0], z[1]] for z in list(G.nodes)}
-    nx.draw(G_largest, positions, node_size=5, ax=ax, node_color="none", edge_color="teal")
+    nx.draw(
+        G_largest, positions, node_size=5, ax=ax, node_color="none", edge_color="teal"
+    )
 
     x, y = zip(*shortest_paths[n][1])
     ax.plot(x, y, c="k", lw=20, alpha=0.5)
@@ -159,23 +189,26 @@ if show_plots:
         s=10,
     )
 
+# %% [markdown]
 # # Link Small Areas to stations
 
+# %%
 small_areas_linked_to_stations_via_network = esb.extract_nearest_dest(
     shortest_paths,
     small_areas_near_g_largest,
-    hv_stations_snapped_to_g_largest
+    hv_stations_snapped_to_g_largest,
 ).drop_duplicates(subset="SMALL_AREA")
 
 
+# %%
 small_areas_remaining_centroids = gpd.GeoDataFrame(
-    small_areas
-    .merge(small_areas_near_g_largest, how="left", indicator=True)
+    small_areas.merge(small_areas_near_g_largest, how="left", indicator=True)
     .query("`_merge` == 'left_only'")
     .drop(columns="_merge")
     .assign(geometry=lambda gdf: gdf.geometry.centroid)
 )
 
+# %%
 small_areas_linked_to_stations_via_nearest = (
     esb.join_nearest_points(small_areas_remaining_centroids, hv_stations_dublin)
     .drop(columns=["geometry", "COUNTYNAME"])
@@ -183,6 +216,7 @@ small_areas_linked_to_stations_via_nearest = (
     .drop_duplicates(subset="SMALL_AREA")
 )
 
+# %%
 small_areas_linked_to_stations = pd.concat(
     [
         small_areas_linked_to_stations_via_network,
@@ -190,9 +224,10 @@ small_areas_linked_to_stations = pd.concat(
     ]
 ).drop_duplicates(subset="SMALL_AREA")
 
+# %%
 if show_plots:
-    
-    f, ax = plt.subplots(figsize=(120,120))
+
+    f, ax = plt.subplots(figsize=(120, 120))
 
     small_areas_linked_to_stations_via_network.plot(
         ax=ax,
@@ -203,7 +238,7 @@ if show_plots:
         lambda x: ax.annotate(
             text=x["station_id"],
             xy=x.geometry.centroid.coords[0],
-            va='bottom',
+            va="bottom",
             color="white",
             path_effects=[pe.withStroke(linewidth=2, foreground="orange")],
         ),
@@ -218,7 +253,7 @@ if show_plots:
         lambda x: ax.annotate(
             text=x["station_id"],
             xy=x.geometry.centroid.coords[0],
-            va='bottom',
+            va="bottom",
             color="white",
             path_effects=[pe.withStroke(linewidth=2, foreground="brown")],
         ),
@@ -236,13 +271,13 @@ if show_plots:
         node_color="none",
         edge_color="red",
     )
-    
+
     hv_stations_dublin.plot(ax=ax, color="black")
     hv_stations_dublin.apply(
         lambda x: ax.annotate(
             text=x["station_id"],
             xy=x.geometry.centroid.coords[0],
-            va='top',
+            va="top",
             color="white",
             path_effects=[pe.withStroke(linewidth=2, foreground="black")],
         ),
@@ -252,7 +287,7 @@ if show_plots:
         lambda x: ax.annotate(
             text=x["station_name"],
             xy=x.geometry.centroid.coords[0],
-            va='bottom',
+            va="bottom",
             color="white",
             path_effects=[pe.withStroke(linewidth=2, foreground="black")],
         ),
@@ -260,12 +295,14 @@ if show_plots:
     )
 
     # plt.legend(["Small Area Station IDs", "CAD Stations linked to Heat Map"], prop={'size': 25});
-    plt.legend(list(range(5)), prop={'size': 25});
+    plt.legend(list(range(5)), prop={"size": 25})
 
+# %% [markdown]
 #
 # # Save
 
+# %%
 small_areas_linked_to_stations.to_file(
-    f"{data_dir}/small-areas-linked-to-map-stations.geojson",
+    data_dir / "small-areas-linked-to-map-stations.geojson",
     driver="GeoJSON",
 )
